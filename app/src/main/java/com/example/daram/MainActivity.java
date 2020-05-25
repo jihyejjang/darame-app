0a789857f74f9c9373ed52653650b84849214e39
jmlee <jmlee2109@naver.com>
2020-05-25 15:11:42 +0900

    버튼누르면 앨범 이동

--- app/src/main/java/com/example/daram/MainActivity.java
+++ app/src/main/java/com/example/daram/MainActivity.java
@@ -1,19 +1,49 @@
 package com.example.daram;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
- dfasldkjf alskdjfa;lskdj fa;slkdjfa;slkdjfxzcv,zcxbvzcxv
-    sjdflsdkjflakjdflkajsdlkfa;sldkfja;sldfj;
+    private final int GET_GALLERY_IMAGE = 200;
+    private ImageView imageview;
+    private ImageView imageview2;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageview = (ImageView) findViewById(R.id.imageView);
+        imageview2= (ImageView) findViewById(R.id.imageView2);
+        imageview.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                startActivityForResult(intent, GET_GALLERY_IMAGE);
+            }
+        });
+
+        imageview2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                startActivityForResult(intent, GET_GALLERY_IMAGE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == GET_GALLERY_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri selectedImageUri = data.getData();
+            imageview.setImageURI(selectedImageUri);
+            imageview2.setImageURI(selectedImageUri);
+        }
+
     }
 }
-
-
-asfasdfafafsfafs;
