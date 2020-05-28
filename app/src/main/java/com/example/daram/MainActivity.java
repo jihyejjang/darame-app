57280658dd9cae11efceed6e6171c1c341a7690d
jihye <jh_r_1004@naver.com>
2020-05-28 19:54:17 +0900

    main화면에서 모자이크 선택 시 갤러리 연동하여 선택한 사진 보여주기

--- app/src/main/java/com/example/daram/MainActivity.java
+++ app/src/main/java/com/example/daram/MainActivity.java
@@ -3,14 +3,21 @@ package com.example.daram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.net.Uri;
+import android.provider.MediaStore;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
-    private final int GET_GALLERY_IMAGE = 200;
+    private static final int REQUEST_CODE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ImageView imageView1 = (ImageView) findViewById(R.id.imageView1) ;
+
         imageView1.setImageResource(R.drawable.darame) ;
 
         Button button=(Button)findViewById(R.id.newActivity);
@@ -39,17 +47,48 @@ public class MainActivity extends AppCompatActivity {
         Button button3=(Button)findViewById(R.id.newActivity3);
         button3.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                Intent intent3=new Intent(Intent.ACTION_VIEW,Uri.parse("content://media/internal/images/media"));
-                startActivityForResult(intent3,GET_GALLERY_IMAGE);
+//                Intent intent3=new Intent(getApplicationContext(),MosaicActivity.class);
+//                startActivity(intent3);
+                Intent intent3 = new Intent(Intent.ACTION_PICK);
+                intent3.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                startActivityForResult(intent3, REQUEST_CODE);
             }
 
         });
     }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if(requestCode == REQUEST_CODE)
+        {
+            if(resultCode == RESULT_OK)
+            {
+                try{
+                    InputStream in = getContentResolver().openInputStream(data.getData());
 
-        if (requestCode == GET_GALLERY_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
-            Uri selectedImageUri = data.getData();
-            imageview.setImageURI(selectedImageUri);
+                    Bitmap img = BitmapFactory.decodeStream(in);
+                    in.close();
+
+//                    ImageView imageView = findViewById(R.id.image);
+//                    imageView.setImageBitmap(img);
+                    Intent intent4 = new Intent(getApplicationContext(), MosaicActivity.class);
+                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                    img.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+                    byte[] byteArray = stream.toByteArray();
+                    intent4.putExtra("image",byteArray);
+                    startActivity(intent4);
+
+
+                }catch(Exception e)
+                {
+
+                }
+            }
+            else if(resultCode == RESULT_CANCELED)
+            {
+                Toast.makeText(this, "사진 선택 취소", Toast.LENGTH_LONG).show();
+            }
         }
+    }
+
 }
