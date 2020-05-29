2e90674782e92b597dfa30a659d3ec79824ab154
jmlee <jmlee2109@naver.com>
2020-05-29 14:43:13 +0900

    지민꺼 끝(소현꺼에 함성추가)

--- app/src/main/java/com/example/daram/CompositeActivity.java
+++ app/src/main/java/com/example/daram/CompositeActivity.java
@@ -1,45 +1,58 @@
 package com.example.daram;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class CompositeActivity extends AppCompatActivity {
 
+    private final int GET_GALLERY_IMAGE = 200;
+    private ImageView imageview;
+    private ImageView imageview2;
+
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_composite);
 
-        ImageView imageView1 = (ImageView) findViewById(R.id.imageView1) ;
-        imageView1.setImageResource(R.drawable.ic_squirrel) ;
+        imageview = (ImageView)findViewById(R.id.imageView);
+        imageview2 = (ImageView)findViewById(R.id.imageView2);
 
-        Button button=(Button)findViewById(R.id.newActivity);
-        button.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Intent intent=new Intent(getApplicationContext(),CameraActivity.class);
-                startActivity(intent);
+        imageview.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent. setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                startActivityForResult(intent, GET_GALLERY_IMAGE);
             }
         });
 
-        Button button2=(Button)findViewById(R.id.newActivity2);
-        button2.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Intent intent2=new Intent(getApplicationContext(),CompositeActivity.class);
-                startActivity(intent2);
+        imageview2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent. setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                startActivityForResult(intent, GET_GALLERY_IMAGE);
             }
         });
+    }
 
-        Button button3=(Button)findViewById(R.id.newActivity3);
-        button3.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Intent intent3=new Intent(getApplicationContext(),MosaicActivity.class);
-                startActivity(intent3);
-            }
-        });
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == GET_GALLERY_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri selectedImageUri = data.getData();
+            imageview.setImageURI(selectedImageUri);
+            imageview2.setImageURI(selectedImageUri);
+
+        }
+
     }
 }
