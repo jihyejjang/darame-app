edf939d3ea6ca2134f5d50aebcc0dcc4615d3bfa
jmlee <jmlee2109@naver.com>
2020-05-29 15:05:17 +0900

    지민꺼 끝(소현꺼에 함성추가)

--- app/src/main/java/com/example/daram/CompositeActivity.java
+++ app/src/main/java/com/example/daram/CompositeActivity.java
@@ -1,32 +1,57 @@
 package com.example.daram;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class CompositeActivity extends AppCompatActivity {
+
+    private final int GET_GALLERY_IMAGE = 200;
+    private ImageView imageview;
+    private ImageView imageview2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_composite);
 
-        Button button=(Button)findViewById(R.id.newActivity);
-        button.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
-                startActivity(intent);
+        imageview = (ImageView)findViewById(R.id.imageView);
+        imageview2 = (ImageView)findViewById(R.id.imageView2);
+        imageview.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent. setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                startActivityForResult(intent, GET_GALLERY_IMAGE);
             }
         });
-        Button button2=(Button)findViewById(R.id.newActivity2);
-        button2.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Intent intent=new Intent(getApplicationContext(),SaveActivity.class);
-                startActivity(intent);
+
+        imageview2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent. setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                startActivityForResult(intent, GET_GALLERY_IMAGE);
             }
         });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == GET_GALLERY_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            Uri selectedImageUri = data.getData();
+            imageview.setImageURI(selectedImageUri);
+            imageview2.setImageURI(selectedImageUri);
+
+        }
+
     }
 }
