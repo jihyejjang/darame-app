27d262a16a5a781c2648f080de4b8d62aa9e0840
sohyeon98720 <47767202+sohyeon98720@users.noreply.github.com>
2020-05-29 16:38:56 +0900

    socket,composite,mosaic Activity merge

--- app/src/main/java/com/example/daram/MainActivity.java
+++ app/src/main/java/com/example/daram/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.daram;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
